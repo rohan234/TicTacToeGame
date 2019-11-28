b8f938ec5945b4e88808f00532f60182da166bed
Noobly387 <34463179+Noobly387@users.noreply.github.com>
2019-11-28 13:46:17 -0800

    Update MainDraw.java

--- MainDraw.java
+++ MainDraw.java
@@ -8,78 +8,85 @@ public class MainDraw extends JComponent {
     public int xPos = 50;
     public int yPos = 50;
     
-    public int width = 50;
-    public int height = 50;
+    public int width = 200;
+    public int height = 200;
     
-    public int textPadding = 15;
+    final int TEXT_PADDING = 15;
     
-    public boolean isGameOver = false;
-    public boolean didPlayerWin = false;
+    Color highlight = new Color(255, 255, 0, 127);
+    
+    void paintText (Graphics g) {
+    	g.drawString("Movement Keys", 600 + TEXT_PADDING, 20);
+        
+		g.drawString("Q: Up and Left", 600 + TEXT_PADDING, 40);
+		g.drawString("W: Up", 600 + TEXT_PADDING, 60);
+		g.drawString("E: Up and Right", 600 + TEXT_PADDING, 80);
+		g.drawString("A: Left", 600 + TEXT_PADDING, 100);
+		g.drawString("S: Sit", 600 + TEXT_PADDING, 120);
+		g.drawString("D: Right", 600 + TEXT_PADDING, 140);
+		g.drawString("Z: Down and Left", 600 + TEXT_PADDING, 160);
+		g.drawString("X: Down", 600 + TEXT_PADDING, 180);
+		g.drawString("C: Down and Right", 600 + TEXT_PADDING, 200);
+		g.drawString("J: Jump", 600 + TEXT_PADDING, 220);
+		g.drawString("ENTER: restart game", 600 + TEXT_PADDING, 240);
+		g.drawString("Objective: Try not to get eaten by the Mhos. Use strategic ", 600 + TEXT_PADDING, 280);
+		g.drawString(" movements to get the mhos to run into fences. Good Luck!", 600 + TEXT_PADDING, 300);
+    }
+    
+    void paintBoard(Graphics g, String[][] board) {
+    	int xPaint;
+        int yPaint;
+        
+        for (int y = 0; y < 3; y++) {
+        	for (int x = 0; x < 3; x++) {
+        		
+        		xPaint = width * x;
+        		yPaint = height * y;
+        		
+        		
+        		if (board[y][x] == "X") { //IF the marker is a fence
+        			g.setColor(Color.orange);
+        			g.fillRect(xPaint,  yPaint,  width, height);
+        		} 
+        		else if (board[y][x] == "O") { //If the marker is the player
+        			g.setColor(Color.black);
+        			g.fillOval(xPaint,  yPaint,  width, height);
+        			
+        			g.setColor(Color.white);
+        			g.fillOval(xPaint + 25,  yPaint + 25,  width / 4 * 3, height / 4 * 3);
+        		} 
+        		else if (board[y][x] == "-") { //If the marker is an empty space
+        			g.setColor(Color.black);
+        			g.drawRect(xPaint,  yPaint,  width, height);
+        		}
+        		if (board[y][x] == "+") {
+        			g.setColor(highlight);
+        			g.fillRect(xPaint, yPaint, width, height);
+        		}
+        		
+        	}
+        }
+    }
+    
+    void paintGameBoard(Graphics g) {
+    	paintBoard(g, Board.gameBoard);
+    }
+    
+    void paintHLBoard(Graphics g) {
+    	paintBoard(g, Board.hlBoard);
+    }
 
     public void paintComponent(Graphics g) {
         
     	super.paintComponent(g);
         
-        int xPaint;
-        int yPaint;
+    	paintGameBoard(g);
         
+        paintHLBoard(g);
         
-        g.drawString("Movement Keys", 600 + textPadding, 20);
-        
-		g.drawString("Q: Up and Left", 600 + textPadding, 40);
-		g.drawString("W: Up", 600 + textPadding, 60);
-		g.drawString("E: Up and Right", 600 + textPadding, 80);
-		g.drawString("A: Left", 600 + textPadding, 100);
-		g.drawString("S: Sit", 600 + textPadding, 120);
-		g.drawString("D: Right", 600 + textPadding, 140);
-		g.drawString("Z: Down and Left", 600 + textPadding, 160);
-		g.drawString("X: Down", 600 + textPadding, 180);
-		g.drawString("C: Down and Right", 600 + textPadding, 200);
-		g.drawString("J: Jump", 600 + textPadding, 220);
-		g.drawString("ENTER: restart game", 600 + textPadding, 240);
-		g.drawString("Objective: Try not to get eaten by the Mhos. Use strategic ", 600 + textPadding, 280);
-		g.drawString(" movements to get the mhos to run into fences. Good Luck!", 600 + textPadding, 300);
-
-
-		
-		//if the game is over and the player won print ... else print ... reset the booleans
-        if(isGameOver) {
-        	g.setColor(Color.BLACK);
-        	g.fillRect(0, 0, 600, 600);
-        	g.setColor(Color.WHITE);
-        	g.drawString(didPlayerWin ? "YOU WON!!!" : "You lost...you are bad: Press enter to restart!", 280, 293);
-        	isGameOver = false;
-        	didPlayerWin = false;
-        	return;
-        }
-        
-        for (int x = 0; x < 12; x++) {
-        	for (int y = 0; y < 12; y++) {
-        		
-        		xPaint = 50 * x;
-        		yPaint = 50 * y;
-        		
-        		if (Board.board[y][x] == "#") { //IF the marker is a fence
-        			g.setColor(Color.orange);
-        			g.fillRect(xPaint,  yPaint,  width, height);
-        		} 
-        		else if (Board.board[y][x] == "+") { //If the marker is the player
-        			g.setColor(Color.black);
-        			g.fillOval(xPaint,  yPaint,  width, height);
-        		} 
-        		else if (Board.board[y][x] == "-") { //If the marker is an empty space
-        			g.setColor(Color.white);
-        			g.fillRect(xPaint,  yPaint,  width, height);
-        		} else if (Board.board[y][x].startsWith("M")) { //If the marker is a Mho
-        			g.setColor(Color.blue);
-        			g.drawRect(xPaint,  yPaint,  width, height);
-        		}  
-        	}
-        }
-        
+        paintText(g);
         
     }
-
 
 }
 
