f6f44f306e277cdaf5eed6685324a3ede455692f
Noobly387 <34463179+Noobly387@users.noreply.github.com>
2019-12-02 21:28:49 -0800

    Update Marker.java
    - Added ability to place marker
    - The things wrok now yay

--- Marker.java
+++ Marker.java
@@ -4,8 +4,10 @@ public class Marker {
 	final int y = 0; //these variables are set to access index in a way so its readable to humans
 	final int x = 1;
 	
-	static int[] pos;
+	static int[] oldPos;
 	static int[] newPos;
+	
+	String playerMarker = "X";
 	
 	static int[] getPosition(String marker, String[][] board) {
 		for (int y = 0; y < 3; y++) {
@@ -16,44 +18,88 @@ public class Marker {
 				}
 			}
 		}
+		
 		System.out.println("ERROR: UNABLE TO FIND MARKER: " + marker);
 		return null;
 	}
 		
 	public void moveUp() {
-		move(/*Axis:*/ y, /*Increase:*/ false);
+		move(findNewPosition(/*Axis:*/ y, /*Increase:*/ false));
 	}
 	
 	public void moveDown() {
-		move(/*Axis:*/ y, /*Increase:*/ true);
+		move(findNewPosition(/*Axis:*/ y, /*Increase:*/ true));
 	}
 	
 	public void moveLeft() {
-		move(/*Axis:*/ x, /*Increase:*/ false);
+		move(findNewPosition(/*Axis:*/ x, /*Increase:*/ false));
 	}
 
 	
 	public void moveRight() {
-		move(/*Axis:*/ x, /*Increase:*/ true);
+		move(findNewPosition(/*Axis:*/ x, /*Increase:*/ true));
 	}
 	
-	private void move(int axis, boolean increase) {
-		pos = getPosition("+", Board.hlBoard);
-		newPos = pos;
+	
+	private int[] findNewPosition(int axis, boolean increase) {
+		oldPos = getPosition("+", Board.hlBoard);
+		newPos = oldPos;
 		
-		newPos[axis] += increase ? 1 : -1;
-		
-		if (validMove(Board.hlBoard, newPos)) {
-			Board.hlBoard[pos[y]][pos[x]] = "-";
-			Board.hlBoard[newPos[y]][newPos[x]] = "+";
+		if (increase) {
+			newPos[axis] += 1;
+		} else {
+			newPos[axis] -= 1;
 		}
+		
+		return newPos;
 	}
+	
+	
+	private void move(int[] newPosition) {
+		
+		if (validMove(Board.hlBoard, newPosition)) {
+			Board.resetBoard(Board.hlBoard);
+			Board.hlBoard[newPosition[y]][newPosition[x]] = "+";
+		}
+		
+		MainFrame.updateVisuals();
+	}
+	
+	
+	public void placeMarker() {
+		
+		oldPos = getPosition("+", Board.hlBoard);
+		
+		if (validMove(Board.gameBoard, getPosition("+", Board.hlBoard))) {
+			Board.gameBoard[oldPos[y]][oldPos[x]] = playerMarker;
+			updatePlayerMarker();
+		}
+			
+		
+		MainFrame.updateVisuals();
+		
+	}
+	
 	
 	private boolean validMove(String[][] board, int[] newPosition) {
-		boolean isValid;
-		isValid = (board[newPosition[y]][newPosition[x]] == "-") ? true : false;
-
-		return isValid;
+		if (board[newPosition[y]][newPosition[x]] == "-") {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	private void updatePlayerMarker() {
+		
+		if (MainFrame.isPlayerOneTurn) {
+			playerMarker = "O";
+			MainFrame.isPlayerOneTurn = false;
+		}
+		else {
+			playerMarker = "X";
+			MainFrame.isPlayerOneTurn = true;
+		}
 		
 	}
 	
