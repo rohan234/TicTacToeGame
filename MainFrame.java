595d926fd319c7ada6cf3984133b1cb05f210867
Noobly387 <34463179+Noobly387@users.noreply.github.com>
2019-12-02 21:29:43 -0800

    Update MainFrame.java

--- MainFrame.java
+++ MainFrame.java
@@ -2,45 +2,85 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JFrame;
 
-
-
-public class MainFrame extends JFrame implements KeyListener{
-    public static MainDraw draw;
-
+public class MainFrame extends JFrame implements KeyListener, MouseListener{
+    
+	public static MainDraw draw;
+    
     final static int FRAME_PADDING = 20;
     final static int TEXT_WIDTH = 400;
     
+    public static boolean isPlayerOneTurn = true;
+    
     Marker highlight = new Marker();
+    
+    static void updateVisuals() {
+    	Board.printBoard(Board.gameBoard);
+    	Board.printBoard(Board.hlBoard);
+    	MainFrame.draw.repaint();
+    }
     
     public void keyPressed(KeyEvent e) {
     	System.out.println("keyPressed");
     }
-
-    void updateVisuals() {
-    	Board.printBoard(Board.gameBoard);
-    	MainFrame.draw.repaint();
-    }
     
     public void keyReleased(KeyEvent e) {
     	
-        	if(e.getKeyCode() == KeyEvent.VK_UP) {
-            	highlight.moveUp();
-            	updateVisuals();
-            } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-            	highlight.moveDown();
-            	updateVisuals();
-            } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-            	highlight.moveLeft();
-            	updateVisuals();
-            } else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            	highlight.moveRight();
-            	updateVisuals();
-            } else if(e.getKeyCode() == KeyEvent.VK_SPACE) {
-            	updateVisuals();
-            } 
+    	if (e.getKeyCode() == KeyEvent.VK_UP) {
+        	highlight.moveUp();
+        	
+        } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+        	highlight.moveDown();
+        	
+        } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+        	highlight.moveLeft();
+        	
+        } else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+        	highlight.moveRight();
+        	
+        } else if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+        	highlight.placeMarker();
+        } 
+        	
+    }
+    
+    public void mouseClicked(MouseEvent e) { 
+  
+        // getClickCount gives the number of quick, 
+        // consecutive clicks made by the user 
+        // show the point where the mouse is i.e 
+        // the x and y coordinates 
+        System.out.println("mouse clicked at point: " + e.getX() + " " + e.getY());
         
+        int[] clickCoordinates = {checkMouseRow(e), checkMouseColumn(e)};
+        
+        Board.resetBoard(Board.hlBoard);
+        Board.addMarker(Board.hlBoard, clickCoordinates[0], clickCoordinates[1], "+");
+        updateVisuals();
+    } 
+    
+    private int checkMouseRow(MouseEvent e) {
+    	if (e.getY() < 200 + 25) {
+        	return 0;
+        } else if (e.getY() < 400 + 25) {
+        	return 1;
+        } else {
+        	return 2;
+        } 
+    }
+    
+    private int checkMouseColumn(MouseEvent e) {
+    	if (e.getX() < 200) {
+        	return 0;
+        } else if (e.getX() < 400) {
+        	return 1;
+        } else {
+        	return 2;
+        } 
     }
     
     
@@ -51,6 +91,7 @@ public class MainFrame extends JFrame implements KeyListener{
     public MainFrame(){
         this.draw = new MainDraw();
         addKeyListener(this);
+        addMouseListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
     }
@@ -79,11 +120,32 @@ public class MainFrame extends JFrame implements KeyListener{
         
     }
     
-    public void winScreen(Graphics g) {
-    	g.setColor(Color.BLACK);
-    	g.fillRect(0, 0, 1000, 1000);
-    	g.drawString("You won!", 480, 493);
-    }
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub	
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
     
  
 }
+
+
+
