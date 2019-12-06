a2ea489f323d6d8f63b5758c1a01b3c45952f286
rohan234 <47093378+rohan234@users.noreply.github.com>
2019-12-06 09:16:43 -0800

    Update GameStatus.java

--- GameStatus.java
+++ GameStatus.java
@@ -1,4 +1,6 @@
 
+
+
 public class GameStatus {
 	
 	boolean playerOneVictory;
@@ -9,7 +11,8 @@ public class GameStatus {
 	//Depending on how the methods below turn out, put on different markers that become lines?
 	
 	public static boolean checkVictory (String board[][]) {
-		if(checkRowVictory(board)[2] == 1)
+		//
+		if(checkRowVictory(board)[2] == 1)// return[0] y coord and return [1] is the x coord of vic pos, return[2] is whether there was victory or not 
 			return true;
 		else if (checkColVictory(board)[2] == 1)
 			return true;
@@ -18,12 +21,14 @@ public class GameStatus {
 		else
 			return false;
 		
+		
+		
 	}
 	
 	public static int[] checkRowVictory(String board[][]) {
 		int[] victoryCoordinate = {0, 0, 0}; //y, x, trueCoordinate
 		
-		
+	// return[0] y coord and return [1] is the x coord of vic pos, return[2] is whether there was victory or not 
 		for (int y = 0; y < 3; y++) {
 			if (board[y][0] == board[y][1] 
 					&& board[y][1] == board[y][2] 
@@ -51,6 +56,7 @@ public class GameStatus {
 	}
 	
 	public static int[] checkDiagVictory(String board[][]) {
+		
 		int[] victoryCoordinate = {0, 0, 0, 0}; //y, x, trueCoordinate, isTopLeft
 		
 		if(board[1][1] != "-") {
@@ -58,12 +64,15 @@ public class GameStatus {
 					&& board[1][1] == board[2][2]) {
 				victoryCoordinate[2] = 1; //sets coordinate as true
 				victoryCoordinate[3] = 1; //sets top left as true
+				
+				MainFrame.testWin.visible = true;
 				return victoryCoordinate;
 		}
 			else if (board[0][2] == board[1][1] 
 					&& board[1][1] == board[2][0]) {
 				victoryCoordinate[2] = 1; //sets coordinate as true
 				victoryCoordinate[1] = 2;
+				MainFrame.testWin.visible = true;
 				return victoryCoordinate;
 			}
 		}
@@ -72,3 +81,4 @@ public class GameStatus {
 	}
 	
 }
+
